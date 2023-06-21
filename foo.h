6543eba897422138b0b5e364d5083523ad750298
BobLynx <boplynx@gmail.com>
2023-06-21 10:00:07 +0900

    Added files

---
+++ foo.h
@@ -0,0 +1,7 @@
+#pragma once
+#include <iostream>
+
+int hello(){
+	std::cout << "mushdovoskiy" << std::endl;
+	return 7032;
+}
---
+++ main.cpp
@@ -0,0 +1,9 @@
+#include <iostream>
+#include "foo.h"
+
+int main() {
+	int num;
+	std::cout << hello() << std::endl;
+	std::cin >> num;
+	return 0;
+}
